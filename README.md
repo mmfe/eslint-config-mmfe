e5abdf99b68e8d684919bf8f0b2b0f3e5332e332
zlxbuzz <824665478@qq.com>
2018-10-08 19:21:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# eslint-config
+
+eslint config for team.
+
+[![npm](https://img.shields.io/npm/v/eslint-config-mmfe.svg)](https://www.npmjs.com/package/eslint-config-mmfe)
+[![GitHub license](https://img.shields.io/github/license/mmfe/eslint-config-mmfe.svg)](https://github.com/mmfe/eslint-config-mmfe/blob/master/LICENSE)
+
+###### 格式化规则以 Prettier 为主，否则可以采用`prettier-eslint`(不建议)
+
+## Usage
+
+```sh
+yarn add --dev eslint eslint-config-mmfe
+```
+
+新建`.eslintrc.js`
+
+```
+module.exports = {
+  "extends": "eslint-config-mmfe"
+}
+```
+
+## License
+
+[**The MIT License**](LICENSE).
---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    commonjs: true
+  },
+  rules: {
+    "default-case": 2, //switch语句中有default分支
+    complexity: 2, //所有代码的独立现行路径条数
+    "no-undef": 0, //禁用未声明的变量
+    "no-unused-vars": 2,
+    "no-empty-function": 2, //禁止出现空函数
+    "no-eq-null": 2, //禁止在没有类型检查操作符的情况下与 null 进行比较
+    "no-unmodified-loop-condition": 2, //禁用一成不变的循环条件
+    "guard-for-in": 2, //要求 for-in 循环中有一个 if 语句
+    "no-alert": 2, //用 alert、confirm 和 prompt
+    "no-else-return": 2 //禁止 if 语句中 return 语句之后有 else 块
+  },
+  parserOptions: {
+    sourceType: "module" //类型为module，因为代码使用了使用了ECMAScript模块
+  },
+  extends: ["plugin:prettier/recommended"]
+};
